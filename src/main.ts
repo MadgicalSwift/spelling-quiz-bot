90d78df437347090f7736db593ec3924b659b487
DeepakJaiz <inspiringdeepak7209@gmail.com>
2024-07-27 11:41:53 +0530

    update endpoint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { LoggingService } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('spellingQuiz/v1');
   await app.listen(3000);
 }
 bootstrap();
